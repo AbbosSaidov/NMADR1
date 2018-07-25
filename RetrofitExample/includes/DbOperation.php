b778e75f5787fef0be524c5a6fb2468eedc13a26
Kimdirde <book19999912@gmail.com>
2018-07-25 18:52:17 +0500

    As

--- RetrofitExample/includes/DbOperation.php
+++ RetrofitExample/includes/DbOperation.php
@@ -1995,7 +1995,6 @@ return $ui;
         $db=new DbOperation();
         $mkjd1 = strlen($data) / 14;
         $kijshda2 = $data;
-        $kijshda3 = "";
 
         for ($lkj = 0; $lkj <$mkjd1; $lkj++)
         {
@@ -2113,7 +2112,6 @@ return $ui;
                 {
                     $db->Sethu3(0,$lk);
                 }
-                //  print("Finally=" + YurishKimmiki[lk].Substring(0, 1) + " " + OxirgiZapisplar[lk, int.Parse(YurishKimmiki[lk].Substring(0, 1))]);
             }
             if (strlen($data) > 9)
             {
@@ -2134,21 +2132,10 @@ return $ui;
             if (strlen($uyinchilar) > 0)
             {
                 if($yurishkimmiki == ""){ $yurishkimmiki = "0"; }
-                if($lkj == 0 && $lkj != $mkjd1 - 1)
+
+                if ($lkj == $mkjd1 - 1 && strpos($uyinchilar,$index)===false)
                 {
-                    $kijshda3 =  $data .substr($yurishkimmiki,0,1) .str_pad($lk,4,"0",STR_PAD_LEFT);
-                }
-                if ($lkj == $mkjd1 - 1)
-                {
-                    $db->SEndMEssageToGroup($lk,$uyinchilar,$kijshda3.$data .substr($yurishkimmiki,0,1) .str_pad($lk,4,"0",STR_PAD_LEFT));
-                    //   Broadcast(kijshda3+data + YurishKimmiki[lk].Substring(0, 1) + lk.ToString().PadLeft(4, '0'), grop2[lk], lk);
-                }
-                else
-                {
-                    if ($lkj != 0)
-                    {
-                        $kijshda3 = $kijshda3.$data .substr($yurishkimmiki,0,1) .str_pad($lk,4,"0",STR_PAD_LEFT);
-                    }
+                    $db->SEndMEssageToGroup($lk,$uyinchilar,$data .substr($yurishkimmiki,0,1) .str_pad($lk,4,"0",STR_PAD_LEFT));
                 }
             }
             if (strlen($uyinchilar) < 2)
